e659b4c61ae4bfb05c51cfd68d0897e10f9be473
AlinaLFox <124040276+AlinaLFox@users.noreply.github.com>
2023-03-10 13:50:43 +0200

    Corectare

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,20 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
 
+        //Adaugarea unor persoane într-un HashSet
         Set<Person> p = new HashSet<>();
         p.add(new Person("Jane", 61));
         p.add(new Person("Michael", 36));
-        p.add(new Person("Basil", 36));
+        p.add(new Person("Basil", 37));
         p.add(new Person("Aly", 45));
-        p.add(new Person("Nick", 61));
+        p.add(new Person("Nick", 62));
         p.add(new Person("Jane", 61)); //incercam sa adaugam duplicat
 
-        for (Person person : p)
-
-        System.out.println("HashSset contains: " + p);
-        System.out.println("Persons older then 60: ");
-
+        //Parcurgerea HashSet-ului și afișarea persoanelor cu vîrsta mai mare de 60 de ani
+        for (Person older : p) {
+            if (older.getAge() > 60) {
+                System.out.println("Persons older then 60: " + older.toString());
+            }
+        }
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,33 +1,60 @@
 public class Person {
-    String name;
-    int age;
+    // declararea variabilelor private
+   private String name;
+   private int age;
 
+    // constructorul pentru clasa Person
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
+
+    // metodele getter si seter pentru ele
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // metoda toString - afiseaza reprezentarea string a obiectului
     @Override // rescriem metoda de baza toString
     public String toString() {
         return "Name: " + name + ", Age: " + age; // unim parametrii pe care dorim ca sa fiu afisate prin metoda asta
     }
+
+    // metoda equals - compara doua obiecte de tip Person
     @Override // rescriem metoda de baza equals
     public boolean equals (Object obj) {
-        if (this == obj) { // compara adresele obiectelor
+        if (this == obj) { // compara adresele obiectelor; // verificam daca cele doua obiecte sunt identice
             return true;
         }
-        Person x = (Person) obj; // adaugam conditiile noastre de comparare
-        if (this.name.equals(x.name) && this.age == x.age) { // compara nume si vairsta daca sunt egale
+
+        Person x = (Person) obj; // castam obiectul la tipul Person
+        if (this.name.equals(x.name) && this.age == x.age) { // verificam daca cele doua obiecte au aceleasi variabile pentru name si age
             return true;
         }
         return false; // daca nici o conditie n-a fost respectata, metoda o sa returneze"false"
 
         /* metoda equals se mai poate fi scrisa prin "instanceof"
          @Override public boolean equals(Object obj) {
-         if ( !(obj instanceof Person)) return false;
-         Person otherPerson = (person) obj;
+         if (obj == this) return true;
+         if ( !(obj instanceof Person)) return false;  // verificam daca obiectul comparat este de tip Person
+         Person other = (person) obj;
          return (this.name.equals(x.name) && this.age == x.age)
          */
     }
+
+    // metoda hashCode - calculeaza valoarea hash a obiectului
     public int hashCode() { //parametru dupa care o sa efectueaza sortarea obiectelor
         return age;
     }
